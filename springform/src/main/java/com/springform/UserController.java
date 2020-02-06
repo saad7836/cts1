87b62765cefbb0a7f13b1a0f19bf5189d2d6ae16
saad7836 <60208897+saad7836@users.noreply.github.com>
2020-02-06 04:22:42 -0800

    Add files via upload

---
+++ springform/src/main/java/com/springform/UserController.java
@@ -0,0 +1,24 @@
+package com.springform;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+	@RequestMapping("/registerForm")
+	public String regUser() {
+		
+		return "regiForm";
+	}
+	@RequestMapping(value="/registerUser", method=RequestMethod.POST)
+	public ModelAndView regiUser(@RequestParam("username") String uname,
+								 @RequestParam("age") int age,
+								 @RequestParam("emil") String emil){
+		User user = new User(uname, age, emil);
+		return new ModelAndView("regSuccess", "user", user);
+	}
+}
+
